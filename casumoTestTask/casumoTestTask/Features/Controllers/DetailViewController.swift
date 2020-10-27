22192b4cf827d1b92091cc40972fd507599fa13b
Oleksii Pelekh <>
2020-10-27 22:05:18 +0200

    Formatted detailed info displaying

--- casumoTestTask/casumoTestTask/Features/Controllers/DetailViewController.swift
+++ casumoTestTask/casumoTestTask/Features/Controllers/DetailViewController.swift
@@ -35,8 +35,13 @@ class DetailViewController: UIViewController {
             eventIdLabel.text = event.id
             eventTypeLabel.text = event.type
             eventCreatedAtLabel.text = event.created_at
-            eventDetailInfoTextField.text = String(describing: event)
+            eventDetailInfoTextField.text = textForTheDetailView(eventDescription: String(describing: event))
             accessoryView.backgroundColor = UIColor.colorForEventType(eventType: event.type)
         }
+    }
+
+    private func textForTheDetailView(eventDescription: String) -> String {
+        // TODO: - should be replaced with an adequate solution
+        return eventDescription.replacingOccurrences(of: "Optional(", with: "").replacingOccurrences(of: "), ", with: "\n - ").replacingOccurrences(of: ", ", with: "\n - ").replacingOccurrences(of: "Event(", with: " - ").replacingOccurrences(of: "casumoTestTask.Repo(", with: "").replacingOccurrences(of: "casumoTestTask.Actor(", with: "").replacingOccurrences(of: "casumoTestTask.Payload(", with: "").replacingOccurrences(of: "))", with: "")
     }
 }
